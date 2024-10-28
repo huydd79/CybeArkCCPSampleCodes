2d934e53c6922fb48fa5ef936c6cde78ace97f20
Huy Do <86530496+huydd79@users.noreply.github.com>
2024-10-28 17:04:29 +0700

    Delete run.sh

--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#RUNNING ARGS: java TestCUNRCCP.java <URL> <timeout> <pfxFile> <pfxPassword> <jksFile> <jksPassword> <disableCertificateValidation>
-java TestCYBRCCP.java \
-    'https://172.16.100.21/AIMWebService/api/Accounts?AppID=TestCCP-HUYDO&Safe=TestingCP_Safe&Folder=Root&Object=testcp01' \
-    /Users/Huy.Do/Documents/HUYDO/Working/Coding/TestCybrCCP/test/clients.cybr.huydo.net.pfx \
-    ChangeMe123! \
-    /Users/Huy.Do/Documents/HUYDO/Working/Coding/TestCybrCCP/test/trust.ccp.jks \
-    ChangeMe123! \
-    true
